deb286201ce97456d51ccdd748889c736c91b9e1
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-09-13 05:30:05 +0300

    added show campaign

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Campaigns from "./components/Campaigns";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ShowCampaign from "./components/ShowCampaign";
+// edit campaing, delete campaing, get campaign stats
 function App() {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App() {
         <Route exact path="/" component={LogIn} />
         <Route exact path="/sign-up" component={SignUp} />
         <Route exact path="/campaigns" component={Campaigns} />
+        <Route exact path="/show-campaign" component={ShowCampaign} />
       </Switch>
     </Router>
   );
