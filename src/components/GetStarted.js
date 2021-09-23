1ebea6dc3687aa4314ca42a50e97d945c82ed423
ALEXANDER <msumba83@gmail.com>
2021-09-23 07:28:05 +0200

    Done Get started css

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
-import "./GetStarted.css";
+// import "./GetStarted.css";
 
 function GetStarted() {
   return (
