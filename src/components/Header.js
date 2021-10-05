35a249b831c3d8cf27506380994d4d43796c71d0
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-10-05 14:26:27 +0300

    merged changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
+import {useHistory} from 'react-router-dom'
 // import MenuIcon from "@mui/icons-material/Menu";
 const Header = () => {
+  const history = useHistory()
   return (
     <section className="get_started_header">
       <img className="get_started_logo" src="/LYC-primary.svg" alt="logo" />
@@ -16,6 +18,7 @@ const Header = () => {
             marginRight: "6px",
           }}
         /> */}
+         <button onClick={() => history.goBack()} className="btn-back">Go Back</button>
     </section>
   );
 };
--- src/components/ShowCampaign.js
+++ src/components/ShowCampaign.js
@@ -86,93 +86,124 @@ const ShowCampaign = () => {
 
   return (
     <React.Fragment>
-       <Header />
-    <div className="sh-group">
-     
-      {campaign ? (
-        <>
-          <div className="sh-header">
-            <h3> {campaign.campaign_title}</h3>
-            {/* <h4>Hosted by: Erick Mulindi</h4> */}
-          </div>
-          <div className="sc-buttons">
-            <button
-              className="cb cb-1 share-3"
-              onClick={() => {
-                navigator.clipboard.writeText(
-                  `https://www.loveyourcity.app/donate/${cId}`
-                );
-                alert("Link Copied");
-              }}
-            >
-              Copy Sharable link
-            </button>
-  
-            <button className="cb cb-2 share-3" onClick={deleteCampaign}>
-              Delete
-            </button>
-            <button className="cb cb-2 disabled">Edit</button>
-          </div>
-          <div className="sh-details-group">
-            <div className="sh-desc">
-              <h3>About this campaign</h3>
-              <p>{campaign.campaign_desc}</p>
+      <Header />
+      <div className="sh-group">
+        {campaign ? (
+          <>
+            <div className="sh-header">
+              <h3> {campaign.campaign_title}</h3>
+              {/* <h4>Hosted by: Erick Mulindi</h4> */}
             </div>
-            <div className="sh-details">
-              <h3 className="sh-desc">Table of items</h3>
-              <div className="table-group">
-
-                <div className="row header-row">
-                  <div className="col">No.</div>
-                  <div className="col">Item Name</div>
-                  <div className="col">Campaign Goal</div>
-                  <div className="col">Donated</div>
-                  <div className="col">Still Needed</div>
-                </div>
-
-                {itemInfo.map((mapItem, index) => {
-                  const { name, goal, donated, needed } = mapItem;
-
-                  return (
-                    <div className="row" key={index}>
-                      <div className="col">{index + 1}</div>
-                      <div className="col">{name}</div>
-                      <div className="col">{goal}</div>
-                      <div className="col">{donated}</div>
-                      <div className="col">{needed}</div>
-                    </div>
+            <div className="sc-buttons">
+              <button
+                className="cb cb-1 share-3"
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `https://www.loveyourcity.app/donate/${cId}`
                   );
-                })}
+                  alert("Link Copied");
+                }}
+              >
+                Copy Sharable link
+              </button>
+
+              <button className="cb cb-2 share-3" onClick={deleteCampaign}>
+                Delete
+              </button>
+              <button className="cb cb-2 disabled">Edit</button>
+            </div>
+            <div className="sh-details-group">
+              <div className="sh-desc">
+                <h3>About this campaign</h3>
+                <p>{campaign.campaign_desc}</p>
               </div>
-              {/* <p className="share-1">Social Share:</p>
+              <div className="sh-details">
+                <h3 className="sh-desc">Table of items</h3>
+                <div className="table-group">
+                  <div className="row header-row">
+                    <div className="col">No.</div>
+                    <div className="col">Item Name</div>
+                    <div className="col">Campaign Goal</div>
+                    <div className="col">Donated</div>
+                    <div className="col">Still Needed</div>
+                  </div>
+
+                  {itemInfo.map((mapItem, index) => {
+                    const { name, goal, donated, needed } = mapItem;
+
+                    return (
+                      <div className="row" key={index}>
+                        <div className="col">{index + 1}</div>
+                        <div className="col">{name}</div>
+                        <div className="col">{goal}</div>
+                        <div className="col">{donated}</div>
+                        <div className="col">{needed}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+                {/* <p className="share-1">Social Share:</p>
           <div className="social-share">
             <FacebookIcon style={{ fontSize: 40 }} />
             <TwitterIcon style={{ fontSize: 40 }} />
             <LinkedInIcon style={{ fontSize: 40 }} />
             <WhatsAppIcon style={{ fontSize: 40 }} />
           </div> */}
+                {campaign.donationItems && (
+                  <>
+                    <h3 className="sh-desc">Donations</h3>
+                    <div className="table-group-donations">
+                      <div className="row header-row">
+                        <div className="col">Gifter</div>
+                        <div className="col">Item</div>
+                        <div className="col">Donated</div>
+                        <div className="col">Donated On</div>
+                      </div>
 
-              <div className="table-group-2">
-                <div className="date">
-                  <p className="header">End Date</p>
-                  <p>{moment(campaign.end_date).format("DD MMMM YYYY")}</p>
-                </div>
-                <div className="address">
-                  <p className="header">Delivery Address</p>
-                  <p>{campaign.delivery_address}</p>
-                </div>
-                <div className="email">
-                  <p className="header">Campaign Owner Email</p>
-                  <p>{campaign.email}</p>
+                      {campaign.donationItems.map((el, index) => {
+                        const {
+                          item_name: name,
+                          item_quantity: quan,
+                          email,
+                          created_at: date,
+                        } = el;
+
+                        return (
+                          <div className="row" key={index}>
+                            <div className="col">{email.split("@")[0]}</div>
+                            <div className="col">{name}</div>
+                            <div className="col">{quan}</div>
+                            <div className="col">
+                              {moment(date).format("DD MMMM YYYY")}
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </>
+                )}
+
+                <div className="table-group-2">
+                  <div className="date">
+                    <p className="header">End Date</p>
+                    <p>{moment(campaign.end_date).format("DD MMMM YYYY")}</p>
+                  </div>
+                  <div className="address">
+                    <p className="header">Delivery Address</p>
+                    <p>{campaign.delivery_address}</p>
+                  </div>
+                  <div className="email">
+                    <p className="header">Campaign Owner Email</p>
+                    <p>{campaign.email}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </>
-      ) : (
-        <LoopRoundedIcon />
-      )}
-    </div>
+          </>
+        ) : (
+          <LoopRoundedIcon />
+        )}
+      </div>
     </React.Fragment>
   );
 };
